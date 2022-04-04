8d1cdad85fe89165d20dd7426400f76f90600776
Alma Hernandez <ahernandez16@hawk.iit.edu>
2022-04-03 19:56:03 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+ function alertFunction() {
+
+   var x1 = document.getElementById("entryinput").value;
+   var x2 = document.getElementById("textoutput");
+
+   alert("Alma Hernandez: "+ x1);
+   x2.innerHTML = x1;
+   
+ }
+
+ var x3 = document.getElementById('entrybutton');
+ x3.addEventListener("click", alertFunction);
 
 
 
-
-
-
-
+}
 window.addEventListener('load', init);
